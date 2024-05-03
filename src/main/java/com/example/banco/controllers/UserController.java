95d2c0c4d66d208c5e1137b602fa65c03ce33c3b
alvsoares1 <alvaroass2110@gmail.com>
2024-05-03 16:01:48 -0300

    Cadastrar Conta, Consulta Saldo

---
+++ src/main/java/com/example/banco/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.example.banco.controllers;
+
+import com.example.banco.entities.User;
+import com.example.banco.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+        User registeredUser = userService.registerUser(user.getNumUser());
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @GetMapping("/{numUser}")
+    public ResponseEntity<User> getUserByNumUser(@PathVariable String numUser) {
+        User user = userService.getUserByNumUser(numUser);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+}
